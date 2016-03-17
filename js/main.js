8d2d81b45a898a416acd34d64856788fa4a56763
Philip Vachon <philipmvachon@gmail.com>
2016-03-17 18:46:53 -0400

    Changed parseInt to parseFloat. Put .toFixed(2) to make all numbers to two decimals.

--- js/main.js
+++ js/main.js
@@ -2,12 +2,12 @@ var totalcost = 0
 
 $("#addItem").click(function() {
 	var item = $("#item").val();
-	var itemcost = parseInt($("#itemcost").val());
+	var itemcost = parseFloat($("#itemcost").val());
 	totalcost = totalcost+itemcost
 	
 
-	$("#itemList").append("<li>" + item + " $" + itemcost + "</li>");
-	$("#totalcost").html(totalcost);
+	$("#itemList").append("<li>" + item + " $" + itemcost.toFixed(2) + "</li>");
+	$("#totalcost").html(totalcost.toFixed(2));
 	document.getElementById('item').value = "";
 	document.getElementById('itemcost').value = "";
 
